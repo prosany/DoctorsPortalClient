f4108bbd692111a94c979afb3b106823e884f42a
Mahabub Sunny <office.prosany@gmail.com>
2021-04-11 20:15:37 +0600

    Some

--- src/Components/HomePage/BusniessInfo/BusniessInfo.js
+++ src/Components/HomePage/BusniessInfo/BusniessInfo.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import InfoCard from '../InfoCard/InfoCard';
+import { faClock, faMapMarkerAlt, faPhone } from '@fortawesome/free-solid-svg-icons';
+
 const infoData = [
     {
         title: 'Opening Hours',
         description: 'We are open 7 days',
-        icon: '',
+        icon: faClock,
         background: 'primary'
     },
     {
         title: 'Visit Our Location',
         description: 'Brooklyn, NY 10036, USA',
-        icon: '',
+        icon: faMapMarkerAlt,
         background: 'dark'
     },
     {
         title: 'Contact Us Now',
         description: '+154293753242',
-        icon: '',
+        icon: faPhone,
         background: 'primary'
     }
 ]
 
 const BusniessInfo = () => {
     return (
-        <section className="row">
-            {
-                infoData.map(info => <InfoCard info={info}></InfoCard>)
-            }
+        <section className="d-flex justify-content-center">
+            <div className="w-75 row">
+                {
+                    infoData.map(info => <InfoCard info={info} key={info.title}></InfoCard>)
+                }
+            </div>
         </section>
     );
 };
--- src/Components/HomePage/InfoCard/InfoCard.js
+++ src/Components/HomePage/InfoCard/InfoCard.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import './InfoCard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const InfoCard = ({info}) => {
+const InfoCard = ({ info }) => {
     return (
-        <div className="col-md-4">
-            <div>
-                <h6>{info.title}</h6>
-                <small>{info.description}</small>
+        <div className="col-md-4 text-white info-card">
+            <div className={`d-flex justify-content-center info-container info-${info.background}`}>
+                <div>
+                    <FontAwesomeIcon className="info-icon" icon={info.icon} />
+                </div>
+                <div>
+                    <h6>{info.title}</h6>
+                    <small>{info.description}</small>
+                </div>
             </div>
         </div>
     );
